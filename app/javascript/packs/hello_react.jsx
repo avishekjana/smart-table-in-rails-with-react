9a0edf2ab2617eccc695e5cbb365772d83d93dff
AVISHEK JANA <avishek@geogo.in>
2021-02-18 12:28:45 +0530

    React app directories created

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -4,12 +4,24 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
+import reducers from '../react/store/reducers';
 import App from "../react/App";
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('reactAppContainer')
   )
 })
