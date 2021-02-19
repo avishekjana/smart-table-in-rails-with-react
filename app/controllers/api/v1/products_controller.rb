4dab8b2f10ece281d4af0a0237b97bb9e0200f02
AVISHEK JANA <avishek@geogo.in>
2021-02-19 10:43:51 +0530

    Smart table with pagination and sorting working

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ProductsController < ApplicationController
   def index
     _sortable = params[:sort].present? ? "#{params[:sort]} #{sort_direction}" : 'created_at desc'
     @page = params[:page] || 1
-    @per = params[:per] || 10
+    @per = params[:per] || 5
     @products = Product.order(_sortable)
     @total_items = @products.count
     @products  = @products.page(@page).per(@per)
--- app/javascript/react/views/products/index.js
+++ app/javascript/react/views/products/index.js
@@ -1,81 +1,13 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import fetchProducts from '../../store/product/actions/fetch_products';
-import SmartTable from './SmartTable';
+import React from 'react';
 
-class Products extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePageNumberClick = this.handlePageNumberClick.bind(this);
-    this.handleSorting = this.handleSorting.bind(this);
-    this.state = {
-      sort: null,
-      sort_dir: null
-    }
-  }
+import ProductsContainer from './ProductsContainer';
 
-  componentDidMount() {
-    this.props.fetchProducts();
-  }
-
-  handlePageNumberClick(page) {
-    const args = { page: page };
-    const { sort, sort_dir } = this.state;
-    if(sort && sort_dir) {
-      args["sort"] = sort
-      args["sort_dir"] = sort_dir
-    }
-    this.props.fetchProducts(args);
-  }
-
-  handleSorting(sort, sort_dir) {
-    this.setState({ sort })
-    this.setState({ sort_dir })
-    const args = {};
-    if(sort && sort_dir) {
-      args["sort"] = sort
-      args["sort_dir"] = sort_dir
-    }
-    this.props.fetchProducts(args);
-  }
-
-  render() {
-    const {
-      isFetching,
-      isError,
-      errorMessage,
-      products,
-      productMeta
-    } = this.props;
-    const isEmpty = products.length === 0;
-
-    if (isEmpty && isFetching) {
-      return <span>Loading</span>;
-    }
-    if (isError) {
-      return <span>{errorMessage}</span>;
-    }
-    if (isEmpty) {
-      return <div className ="default-div"><h5>No product found with the current applied filters.</h5><h6>Try applying different filters.</h6></div>;
-    }
-    return (
-      <SmartTable
-        products={products}
-        productMeta={productMeta}
-        handlePageNumberClick={this.handlePageNumberClick}
-        handleSorting={this.handleSorting}
-    />
+const Products = props => {
+  return (
+    <div>
+      <h4>Listing Products</h4>
+      <ProductsContainer />
+    </div>
   );
-  }
 }
-function mapStateToProps(state) {
-  return {
-    products: state.product.all,
-    productMeta: state.product.meta,
-    isFetching: state.product.isFetching,
-    isError: state.product.isError,
-    errorMessage: state.product.errorMessage
-  };
-}
-export default connect(mapStateToProps, { fetchProducts })(Products);
-
+export default Products;
